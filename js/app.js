7a65b574818a6637560cb93fb8e50b964bbe2469
wanessasl <56557812+wanessasl@users.noreply.github.com>
2021-04-12 22:18:55 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -116,4 +116,13 @@ function deleteTodo(key){
     renderTodo(todo);
 
 }   
+document.addEventListener('DOMContentLoaded', () => {
+    const ref = localStorage.getItem('todoItemsRef');
+    if (ref) {
+      todoItems = JSON.parse(ref);
+      todoItems.forEach(t => {
+        renderTodo(t);
+      });
+    }
+  });
 
